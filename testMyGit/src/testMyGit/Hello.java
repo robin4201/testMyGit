26272be1eba1394b310623d8a97bbd540c9413c3
codetalk <bigbigrobin@hotmail.com>
2017-05-09 15:26:26 +0800

    v0.1

---
+++ testMyGit/src/testMyGit/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testMyGit;
+
+/**
+ * @author JR
+ * v 0.1
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
