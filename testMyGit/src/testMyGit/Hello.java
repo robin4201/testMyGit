1023f7ca8232d14e40dc9d84573598c6689d61d4
robin <bigbigrobin@hotmail.com>
2020-10-16 10:46:25 +0800

    test 1

--- testMyGit/src/testMyGit/Hello.java
+++ testMyGit/src/testMyGit/Hello.java
@@ -15,7 +15,10 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//test0
 		System.out.println("Hello");
+		//test
+		//test2
 	}
 
 }
